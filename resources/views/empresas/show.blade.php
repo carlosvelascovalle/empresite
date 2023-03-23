6df5b7e5bc681ee6835cb8af2302e2b5aeb6ce93
carlosvelascovalle <carlosvelascovalle@gmail.com>
2023-03-23 19:45:57 +0100

    Header SEO 1.1

--- resources/views/empresas/show.blade.php
+++ resources/views/empresas/show.blade.php
@@ -1,5 +1,36 @@
-@include('headerinteriores')
+@include('headersuperior')
+<title>{{ $empresa->denominacion }} | EMPRESITE</title>
+<meta name="description"
+    content="Descubre {{ $empresa->denominacion }} en el listado de empresas Empresite. ¡No busques más, Empresite es tu guía para encontrar lo que necesitas en el mundo empresarial!">
+@include('header')
+<header class="site-content-page-header"
+    style="background: url(/img/listado-de-empresas.jpg) center center no-repeat fixed">
+    <div class="site-content-page-header-inner">
+        <div class="company-search-block">
+            <div class="section-header">
 
+            </div>
+            <div class="company-search-form">
+                <form method="GET" action="/empresas/">
+                    <div class="company-search-keywords">
+                        <label class="sr-only" for="search">Keywords</label>
+                        <input type="text" id="search" name="search"
+                            placeholder="Busca por CIF, Nombre de Empresa, Provincia..." class="ui-autocomplete-input"
+                            autocomplete="off" value="{{ $search }}">
+                    </div>
+                    <div class="company-search-submit">
+                        <button type="submit" value="Search"><i class="la la-search"></i><span
+                                class="company-search-text">Search</span></button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <div class="jh-scroll-to">
+            <a href="#jh-scroll-here" title="Scroll Button"><i class="la la-arrow-down"></i></a>
+        </div>
+    </div>
+</header>
 <div id="content" class="site-content" tabindex="-1">
     <div class="container">
         <div class="site-content-inner">
