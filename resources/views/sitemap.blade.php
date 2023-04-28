054434dc0918553c3a15936f7bb5fb72155b23b6
carlosvelascovalle <carlosvelascovalle@gmail.com>
2023-04-28 13:36:59 +0200

    sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,12 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                @foreach ($empresas as $empresa)
-	@php($slug = \Str::slug($empresa->denominacion, '-', 'es'))
-                    <url>
-                        <loc>{{ url('/') }}/empresa/{{ $slug }}</loc>
-                        <lastmod>2023-01-01</lastmod>
-                    </url>
-                @endforeach
+    @foreach ($empresas as $empresa)
+        @php($slug = \Str::slug($empresa->denominacion, '-', 'es'))
+        <url>
+            <loc>{{ url('/') }}/{{ str_replace(' ', '-', strtolower($empresa->provincia)) }}/{{ $empresa->slug }}
+            </loc>
+            <lastmod>2023-01-01</lastmod>
+        </url>
+    @endforeach
 </urlset>
-
-
